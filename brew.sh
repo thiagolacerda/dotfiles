5691656c502fbac3d859331abca8fe6d4516b3aa
Thiago Lacerda <thiagotbl@gmail.com>
2018-05-27 00:36:13 -0700

    Add ctags-exuberant to homebrew list of packages to install

--- brew.sh
+++ brew.sh
@@ -29,6 +29,8 @@ brew install homebrew/cask/sublime-text
 
 brew install libnotify
 
+brew install ctags-exuberant
+
 # Switch to using brew-installed bash as default shell
 if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
     echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
