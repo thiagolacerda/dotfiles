43d91baca56629c5b22be51277713959cdb5f260
Thiago Lacerda <thiagotbl@gmail.com>
2018-07-30 22:12:51 -0700

    Reliable way to get the correct path of the scripts dir

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-scripts_dir=`dirname ${BASH_SOURCE}`;
+scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "${scripts_dir}";
 
 #git pull origin master;
